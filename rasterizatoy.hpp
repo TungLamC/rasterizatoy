b94de49bb13fee0a8bd1fbce878a8ea9367e2395
donglin.shi <shidonglin@bytedance.com>
2021-08-02 10:46:00 +0800

    数学库: 向量与向量的四则运算

---
+++ rasterizatoy.hpp
@@ -0,0 +1,183 @@
+#ifndef RASTERIZATOY_HPP
+#define RASTERIZATOY_HPP
+
+#include <tuple>
+#include <cassert>
+#include <utility>
+#include <initializer_list>
+
+namespace rasterizatoy
+{
+template<size_t N, typename T> class Vector;
+
+template<typename T> using Vector2 = Vector<2, T>;
+template<typename T> using Vector3 = Vector<3, T>;
+template<typename T> using Vector4 = Vector<4, T>;
+}
+
+namespace rasterizatoy
+{
+//-------------------------------------------------------- math --------------------------------------------------------
+
+namespace derived
+{
+template<typename T> struct VectorN { T data[2]; };
+template<typename T> struct Vector2 { union { struct { T x, y; }; struct { T u, v; }; T data[2]; }; };
+template<typename T> struct Vector3 { union { struct { T x, y, z; }; struct { T r, g, b; }; T data[3]; }; };
+template<typename T> struct Vector4 { union { struct { T x, y, z, w; }; struct { T r, g, b, a; }; T data[4]; }; };
+}
+
+template<size_t N, typename T>
+class Vector: public
+  std::conditional_t<N == 2, derived::Vector2<T>,
+  std::conditional_t<N == 3, derived::Vector3<T>,
+  std::conditional_t<N == 4, derived::Vector4<T>,
+  derived::VectorN<T>>>>
+{
+public:
+  inline Vector() { for (size_t i = 0; i < N; i++) data_[i] = T{}; }
+  inline Vector(const T* pointer) { for (size_t i = 0; i < N; i++) data_[i] = pointer[i]; }
+  inline Vector(const Vector<N, T>& other) { for (size_t i = 0; i < N; i++) data_[i] = other.data_[i]; }
+  inline Vector(const std::initializer_list<T>& initializer) { for (size_t i = 0; i < N; i++) data_[i] = *(initializer.begin() + i); }
+
+  inline T& operator[](size_t index) { assert(index < N); return data_[index]; }
+  inline const T& operator[](size_t index) const { assert(index < N); return data_[index]; }
+
+  inline Vector<N, T> normalize() { return *this / length(); }
+  inline Vector<N, T> normalize(std::in_place_t) { return *this /= length(); }
+  inline T length(bool square = false) const { T result = 0; for (size_t i = 0; i < N; i++) result += data_[i] * data_[i]; return result; }
+
+private:
+  T (&data_)[N] = this->data;
+};
+
+template<size_t N, typename T>
+inline Vector<N, T> operator+(const Vector<N, T>& vector)
+{
+  return vector;
+}
+
+template<size_t N, typename T>
+inline Vector<N, T> operator-(const Vector<N, T>& vector)
+{
+  Vector<N, T> result;
+  for (size_t i = 0; i < N; i++) result[i] = -vector[i];
+  return result;
+}
+
+template<size_t N, typename T>
+inline Vector<N, T> operator==(const Vector<N, T>& lhs, const Vector<N, T>& rhs)
+{
+  for (size_t i = 0; i < N; i++) if (lhs[i] != rhs[i]) return false;
+  return true;
+}
+
+template<size_t N, typename T>
+inline Vector<N, T> operator!=(const Vector<N, T>& lhs, const Vector<N, T>& rhs)
+{
+  return !(lhs == rhs);
+}
+
+template<size_t N, typename T>
+inline Vector<N, T> operator+(const Vector<N, T>& lhs, const Vector<N, T>& rhs)
+{
+  Vector<N, T> result;
+  for (size_t i = 0; i < N; i++) result[i] = lhs[i] + rhs[i];
+  return result;
+}
+
+template<size_t N, typename T>
+inline Vector<N, T>& operator+=(Vector<N, T>& lhs, const Vector<N, T>& rhs)
+{
+  for (size_t i = 0; i < N; i++) lhs[i] = lhs[i] + rhs[i];
+  return lhs;
+}
+
+template<size_t N, typename T>
+inline Vector<N, T> operator-(const Vector<N, T>& lhs, const Vector<N, T>& rhs)
+{
+  return lhs + (-rhs);
+}
+
+template<size_t N, typename T>
+inline Vector<N, T>& operator-=(Vector<N, T>& lhs, const Vector<N, T>& rhs)
+{
+  return lhs += (-rhs);
+}
+
+template<size_t N, typename T>
+inline Vector<N, T> operator*(const Vector<N, T>& lhs, const Vector<N, T>& rhs)
+{
+  Vector<N, T> result;
+  for (size_t i = 0; i < N; i++) result[i] = lhs[i] * rhs[i];
+  return result;
+}
+
+template<size_t N, typename T>
+inline Vector<N, T> operator*(const Vector<N, T>& vector, T scalar)
+{
+  Vector<N, T> result;
+  for (size_t i = 0; i < N; i++) result[i] = vector[i] * scalar;
+  return result;
+}
+
+template<size_t N, typename T>
+inline Vector<N, T> operator*(T scalar, const Vector<N, T>& vector)
+{
+  return vector * scalar;
+}
+
+template<size_t N, typename T>
+inline Vector<N, T>& operator*=(Vector<N, T>& lhs, const Vector<N, T>& rhs)
+{
+  for (size_t i = 0; i < N; i++) lhs[i] = lhs[i] * rhs[i];
+  return lhs;
+}
+
+template<size_t N, typename T>
+inline Vector<N, T>& operator*=(Vector<N, T>& vector, T scalar)
+{
+  for (size_t i = 0; i < N; i++) vector[i] = vector[i] * scalar;
+  return vector;
+}
+
+template<size_t N, typename T>
+inline Vector<N, T> operator/(const Vector<N, T>& lhs, const Vector<N, T>& rhs)
+{
+  Vector<N, T> result;
+  for (size_t i = 0; i < N; i++) result[i] = lhs[i] / rhs[i];
+  return result;
+}
+
+template<size_t N, typename T>
+inline Vector<N, T> operator/(const Vector<N, T>& vector, T scalar)
+{
+  Vector<N, T> result;
+  for (size_t i = 0; i < N; i++) result[i] = vector[i] / scalar;
+  return result;
+}
+
+template<size_t N, typename T>
+inline Vector<N, T> operator/( T scalar, const Vector<N, T>& vector)
+{
+  Vector<N, T> result;
+  for (size_t i = 0; i < N; i++) result[i] = scalar / vector;
+  return result;
+}
+
+template<size_t N, typename T>
+inline Vector<N, T>& operator/=(Vector<N, T>& lhs, const Vector<N, T>& rhs)
+{
+  for (size_t i = 0; i < N; i++) lhs[i] = lhs[i] / rhs[i];
+  return lhs;
+}
+
+template<size_t N, typename T>
+inline Vector<N, T>& operator/=(const Vector<N, T>& vector, T scalar)
+{
+  for (size_t i = 0; i < N; i++) vector[i] = vector[i] / scalar;
+  return vector;
+}
+}
+
+#endif //RASTERIZATOY_HPP
